ba0e0ad87e547242c8927ee4c83f840b8864d2f0
Daniil Kornach <danik.kornach@gmail.com>
2023-06-07 12:26:46 +0300

    Выполнил программу с использованием диапазонного цикла

--- Task 2/Task 2.cpp
+++ Task 2/Task 2.cpp
@@ -1,1 +1,15 @@
-﻿
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	char string[] = { "JGMXCIJPAINKALNTHQNMA" };
+	int size = sizeof(string);
+	sort(string, string + size);
+	cout << "Result: ";
+	
+	for (char c : string) {
+		cout << c << ' ';
+	}
+	return 0;
+}
